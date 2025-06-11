01cfd849b8d934252873536f3b8812219f006cad
RadyaAlbasha <radya.albasha15@gmail.com>
2025-06-11 23:38:01 +0300

    create new project

---
+++ AppleFrameworks-SwiftUI/AppleFrameworks-SwiftUI/AppleFrameworks_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworks_SwiftUIApp.swift
+//  AppleFrameworks-SwiftUI
+//
+//  Created by Radya Albasha on 11/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworks_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
