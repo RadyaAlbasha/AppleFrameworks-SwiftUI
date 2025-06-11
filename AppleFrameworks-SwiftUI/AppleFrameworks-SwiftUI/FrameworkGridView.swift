27998123c9b277b1123b0ba067c180b01ca63ac0
RadyaAlbasha <radya.albasha15@gmail.com>
2025-06-12 02:03:07 +0300

    add mocked data and create the first view

---
+++ AppleFrameworks-SwiftUI/AppleFrameworks-SwiftUI/FrameworkGridView.swift
@@ -0,0 +1,54 @@
+//
+//  FrameworkGridView.swift
+//  AppleFrameworks-SwiftUI
+//
+//  Created by Radya Albasha on 11/06/2025.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+    var body: some View {
+        NavigationView {
+            ScrollView{
+                LazyVGrid(columns: columns) {
+                    ForEach(MockData.frameworks){ framework in
+                        FrameworkTitleView(framework: framework)
+                    }
+                    
+                }
+            }
+            .navigationTitle("🍎 Frameworks")
+        }
+        
+        
+    }
+}
+
+#Preview {
+    FrameworkGridView()
+}
+
+struct FrameworkTitleView: View {
+    
+    let framework: Framework
+    
+    var body: some View {
+        VStack{
+            Image(framework.imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            
+            Text(framework.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }
+        .padding()
+    }
+}
